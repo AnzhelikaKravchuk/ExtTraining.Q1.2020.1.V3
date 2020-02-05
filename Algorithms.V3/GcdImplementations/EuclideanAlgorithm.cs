ac66ed587f530aa286d560b7c9ea09eecd9379c1
AnzhelikaKravchuk <anzhelika.kravchuk@gmail.com>
2020-02-05 20:52:55 +0300

    Initial commit

---
+++ Algorithms.V3/GcdImplementations/EuclideanAlgorithm.cs
@@ -0,0 +1,12 @@
+using Algorithms.V3.Interfaces;
+
+namespace Algorithms.V3.GcdImplementations
+{
+    public class EuclideanAlgorithm : IAlgorithm
+    {
+        public int Calculate(int first, int second)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
